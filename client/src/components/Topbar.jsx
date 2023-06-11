fe7cfe826d0e955edff16507fb9e577604e08ed9
Rcatarata <69015151+Rcatarata@users.noreply.github.com>
2023-06-11 10:10:44 -0400

    added dropdown logout
    logout from top right corner

--- client/src/components/Topbar.jsx
+++ client/src/components/Topbar.jsx
@@ -3,18 +3,47 @@ import { useSelector, useDispatch } from 'react-redux'
 import SearchBar from './SearchBar'
 import { Link } from 'react-router-dom'
 import defaultCat from '../pictures/defaultCat.jpeg'
+import '../styles/components/TopBar.css'
+import { AiOutlineLogout } from 'react-icons/ai'
+import { logout, reset} from '../features/auth/authSlice'
+import { reset as reset2 } from '../features/posts/postSlice'
+import {reset as resetChat} from '../features/chat/chatSlice'
+import {reset as resetUsers} from '../features/users/userSlice'
+import { useNavigate } from 'react-router-dom'
 
 export default function Topbar() {
     const {user} = useSelector((state)=> state.auth)
+    const dispatch = useDispatch()
+    const navigate = useNavigate()
 
+    const onLogout = () => {
+      navigate('/login')
+      dispatch(logout())
+        dispatch(reset())
+        dispatch(reset2())
+        dispatch(resetChat())
+        dispatch(resetUsers())
+      // dispatch(reset())
+      // dispatch(reset2())
+      // dispatch(resetChat())
+      // dispatch(resetUsers())
+      // navigate('/login')
+  }
 
   return (
     <div className='home--top--bar gone'>
           <SearchBar className='margin--left'/>
-          <Link to={`/users/${user.id}`}>
-            {user && <img src={user.profileImage ? user.profileImage:defaultCat} className='profile--pic--top' />}
-            {user && <p>{user.firstName + ' ' + user?.lastName}</p>}
-          </Link>
+          <div className='top--bar--right'>
+            <Link to={`/users/${user.id}`}>
+              {user && <img src={user.profileImage ? user.profileImage:defaultCat} className='profile--pic--top' />}
+              {user && <p>{user.firstName + ' ' + user?.lastName}</p>}
+            </Link>
+            <div className='top--right--drop'>
+              <div className='top--right--inner' onClick={onLogout}>
+                <AiOutlineLogout/> <span>Logout</span>
+              </div>
+            </div>
+          </div>
     </div>
   )
 }
